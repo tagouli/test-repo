efe2122f3916615c1a19b7c0d796882978b0c202
tagouli <tagouli3@gmail.com>
2024-06-27 20:37:30 +0100

    c++ file

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,sum;
+cout<<"entre two number to calculate sum"<<endl;
+cin>>a>>b;
+sum=a+b;
+cout<<"the sum of "<<a<<"and"<<b<<"are:"<<sum<<endl;
+return 0;
+
+}
